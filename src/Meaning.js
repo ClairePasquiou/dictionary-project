55272200b63d27f41cdd8315f2d26e2fb570b1ab
Claire Pasquiou <clairepasquiou@hotmail.com>
2023-07-05 12:54:11 +0200

    rendered the word meanings

---
+++ src/Meaning.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function Meaning(props) {
+  console.log(props.meaning);
+  return (
+    <div className="Meaning">
+      <h3>{props.meaning.partOfSpeech}</h3>
+     <div className="definition">
+      {props.meaning.definition}
+     </div>
+     
+    </div>
+  );
+}
