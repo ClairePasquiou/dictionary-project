63fc88f2cb5089499e05406e0e5db106a96d96c1
Claire Pasquiou <clairepasquiou@hotmail.com>
2023-07-17 11:55:44 +0200

    Added CSS to synonyms component

--- src/Synonyms.js
+++ src/Synonyms.js
@@ -1,17 +1,17 @@
 import React from "react";
+import "./Synonyms.css";
 
-export default function Synonyms(props){
-  if (props.synonyms){
+export default function Synonyms(props) {
+  if (props.synonyms) {
     return (
       <div className="Synonyms">
         <ul className="Synonyms">
-          {props.synonyms.map(function(synonym, index){
+          {props.synonyms.map(function (synonym, index) {
             return <li key={index}>{synonym}</li>;
           })}
-
         </ul>
       </div>
-    )
+    );
   } else {
     return null;
   }
