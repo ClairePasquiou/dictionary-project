b2845a1b3c3ff0a79abbf7493b2e908c6ecae588
Claire Pasquiou <clairepasquiou@hotmail.com>
2023-07-17 11:52:55 +0200

    Created synonym component

--- src/Meaning.js
+++ src/Meaning.js
@@ -1,16 +1,20 @@
 import React from "react";
 import Example from "./Example.js";
+import Synonyms from "./Synonyms.js";
 
 export default function Meaning(props) {
   console.log(props.meaning);
   return (
     <div className="Meaning">
       <h3>{props.meaning.partOfSpeech}</h3>
-     <div className="definition">
-      {props.meaning.definition}
-     </div>
-     <Example example={props.meaning.example}/>
-     
+      <div className="definition">
+        
+        {props.meaning.definition}
+      </div>
+      <Example example={props.meaning.example} />
+      <div>
+         <Synonyms synonyms={props.meaning.synonyms} />
+      </div>
     </div>
   );
 }
