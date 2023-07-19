0e8798a6e225a513cc8d3dcaf0f651f4864c8dc3
Claire Pasquiou <clairepasquiou@hotmail.com>
2023-07-19 14:09:11 +0200

    Added alt text to fix netlify deploy error

--- src/Photos.js
+++ src/Photos.js
@@ -8,13 +8,14 @@ export default function Photos(props) {
       <section className="Photos">
         <div className="row">
           {props.photos.map(function (photo, index) {
-            return ( 
+            return (
               <div className="col-4">
                 <a href={photo.src.original} target="_blank" rel="noreferrer">
                   <img
                     src={photo.src.landscape}
                     key={index}
                     className="img-fluid"
+                    alt={photo.alt}
                   />
                 </a>
               </div>
