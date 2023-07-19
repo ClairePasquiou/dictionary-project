addc7b348ff241c9c5dc81d1199707f858d86443
Claire Pasquiou <clairepasquiou@hotmail.com>
2023-07-19 13:27:18 +0200

    Added photo API and added CSS for photo.js

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,60 +1,72 @@
 import React, { useState } from "react";
 import axios from "axios";
 import Results from "./Results";
+import Photos from "./Photos";
 import "./Dictionary.css";
 
 export default function Dictionary(props) {
   let [keyword, setKeyword] = useState(props.defaultKeyword);
   let [results, setResults] = useState(null);
   let [loaded, setLoaded] = useState(false);
+  let [photos, setPhotos] = useState(null);
 
   function handleResponse(response) {
     setResults(response.data);
   }
 
-  function search(){
+  function handlePexelsResponse(response) {
+    setPhotos(response.data.photos);
+  }
+
+  function search() {
     //documentation : https://www.shecodes.io/learn/apis/dictionary
+    //documentation photos API: https://www.shecodes.io/learn/apis/images
 
     let apiKey = "b6c3674af95t190o44befdaa67cd0242";
 
     let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${apiKey}`;
     axios.get(apiUrl).then(handleResponse);
+
+    let pexelsApiKey = "b6c3674af95t190o44befdaa67cd0242";
+    let pexelsApiUrl = `https://api.shecodes.io/images/v1/search?query=${keyword}&key=${pexelsApiKey}`;
+    axios.get(pexelsApiUrl).then(handlePexelsResponse);
   }
 
   function handleSubmit(event) {
     event.preventDefault();
-search();
-    
+    search();
   }
-
-
-
 
   function handleKeywordChange(event) {
     setKeyword(event.target.value);
   }
 
-function load (){
-  setLoaded(true);
-  search();
-}
+  function load() {
+    setLoaded(true);
+    search();
+  }
 
-if (loaded) {
-  return (
-    <div className="Dictionary">
+  if (loaded) {
+    return (
+      <div className="Dictionary">
         <h1>What word are you looking for?</h1>
 
-      <section>
-        <form onSubmit={handleSubmit}>
-          <input type="search" onChange={handleKeywordChange} defaultValue={props.defaultKeyword}/>
-        </form>
-        <div className="hint">i.e: sunset, books, toronto, running</div>
-      </section>
-      <Results results={results} />
-    </div>
-  );} else {
+        <section>
+          <form onSubmit={handleSubmit}>
+            <input
+              type="search"
+              onChange={handleKeywordChange}
+              defaultValue={props.defaultKeyword}
+            />
+          </form>
+          <div className="hint">i.e: sunset, books, toronto, running</div>
+        </section>
+        <Results results={results} />
+        <Photos photos={photos} />
+      </div>
+    );
+  } else {
     load();
     return "Loading";
   }
-  
 }
